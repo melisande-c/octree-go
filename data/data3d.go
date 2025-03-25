150fc96e476490b8eac1d5d38b90ef9c7252b4df
melisande-c <melisande.croft@fht.org>
2025-03-25 20:13:55 +0100

    perf: small 3d slicing improvement

--- data/data3d.go
+++ data/data3d.go
@@ -25,29 +25,27 @@ func (d *BinData3D) All() bool {
 
 // Get value at (i, j, k)
 func (d *BinData3D) Get(i, j, k int) bool {
-	return d.Data[i*d.X*d.Z+j*d.Z+k]
+	return d.Data[i*d.Y*d.Z+j*d.Z+k]
 }
 
-func (d *BinData3D) GetSlice(i, i_extent, j, j_extent, k, k_extent int) BinData3D {
+func (d *BinData3D) GetSlice(i, iExtent, j, jExtent, k, kExtent int) BinData3D {
 	// TODO: make sure that extents are not greater than d.X d.Y d.Z
-	if i+i_extent >= d.X {
-		i_extent = d.X - i
+	if i+iExtent >= d.X {
+		iExtent = d.X - i
 	}
-	if j+j_extent >= d.Y {
-		j_extent = d.Y - j
+	if j+jExtent >= d.Y {
+		jExtent = d.Y - j
 	}
-	if k+k_extent >= d.Z {
-		k_extent = d.Z - k
+	if k+kExtent >= d.Z {
+		kExtent = d.Z - k
 	}
-	slice_data := make([]bool, i_extent*j_extent*k_extent)
-	index := 0
-	for l := i; l < i+i_extent; l++ {
-		for m := j; m < j+j_extent; m++ {
-			for n := k; n < k+k_extent; n++ {
-				slice_data[index] = d.Get(l, m, n)
-				index++
-			}
+	slice := make([]bool, iExtent*jExtent*kExtent)
+	for l := i; l < i+iExtent; l++ {
+		for m := j; m < j+jExtent; m++ {
+			sd := l*d.Y*d.Z + m*d.Z
+			s := (l-i)*jExtent*kExtent + (m-j)*kExtent
+			copy(slice[s:s+kExtent], d.Data[sd+k:sd+k+kExtent])
 		}
 	}
-	return BinData3D{Data: slice_data, X: i_extent, Y: j_extent, Z: k_extent}
+	return BinData3D{Data: slice, X: iExtent, Y: jExtent, Z: kExtent}
 }
