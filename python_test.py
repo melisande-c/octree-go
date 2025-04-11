b1538933aed060c35dea359c143d3023b50164c3
melisande-c <melisande.croft@fht.org>
2025-04-11 18:06:22 +0200

    refac: clean up python test

--- python_test.py
+++ python_test.py
@@ -1,178 +1,26 @@
 import os
 import time
 from typing import Optional
+import ctypes
 
 import numpy as np
+from numpy.typing import NDArray
+import numpy.ctypeslib as npct
 import matplotlib.pyplot as plt
 import tifffile
 
-import ctypes
-
-from numpy.typing import NDArray
-import numpy.ctypeslib as npct
-
-import ctypes
+from pyoctree.octree import Tree
 
 print(os.getcwd())
-
-# golib = ctypes.CDLL("libgo.so")
-# golib = npct.load_library("libgo_amd64.so", ".")
-golib = ctypes.cdll.LoadLibrary("./libgo_amd64.so")
-array_1d_int = npct.ndpointer(dtype=np.int32, ndim=1, flags="CONTIGUOUS")
-
-# Define function signatures
-golib.NewOcTree.argtypes = [array_1d_int, ctypes.c_int, ctypes.c_int, ctypes.c_int]
-golib.NewOcTree.restype = ctypes.c_void_p
-
-golib.DeleteOcTree.argtypes = [ctypes.c_void_p]
-golib.DeleteOcTree.restype = None
-
-golib.FindMinDist.restype = None
-golib.FindMinDist.argtypes = [
-    ctypes.c_void_p,
-    ctypes.c_int,
-    ctypes.c_int,
-    ctypes.c_int,
-    ctypes.c_int,
-    ctypes.c_int,
-    ctypes.c_int,
-    ctypes.c_double,
-    ctypes.c_double,
-    ctypes.c_double,
-    ctypes.POINTER(ctypes.c_double),
-    ctypes.POINTER(ctypes.c_int),
-    ctypes.POINTER(ctypes.c_int),
-    ctypes.POINTER(ctypes.c_int),
-]
-
-
-class Tree:
-    def __init__(self, bin_data: NDArray[np.int_]):
-        shape_c = (ctypes.c_int * 3)(*bin_data.shape)
-        self.ptr = golib.NewOcTree(
-            bin_data.flatten(), shape_c[0], shape_c[1], shape_c[2]
-        )  # uintptr in Go
-
-    def __del__(self):
-        golib.DeleteOcTree(self.ptr)
-
-    def find_min_dist(
-        self,
-        coords: tuple[int, int, int],
-        offset: Optional[tuple[int, int, int]] = None,
-        scaling: Optional[tuple[float, float, float]] = None,
-    ) -> tuple[float, tuple[int, int, int]]:
-        if offset is None:
-            offset = (0, 0, 0)
-        if scaling is None:
-            scaling = (1, 1, 1)
-
-        out_dist = ctypes.c_double(-1)
-        out_dist_ptr = ctypes.pointer(out_dist)
-
-        out_loc_x = ctypes.c_int(-1)
-        out_loc_y = ctypes.c_int(-1)
-        out_loc_z = ctypes.c_int(-1)
-        out_loc_x_ptr = ctypes.pointer(out_loc_x)
-        out_loc_y_ptr = ctypes.pointer(out_loc_y)
-        out_loc_z_ptr = ctypes.pointer(out_loc_z)
-
-        coords_c = (ctypes.c_int * 3)(*coords)
-        offset_c = (ctypes.c_int * 3)(*offset)
-        scaling_c = (ctypes.c_double * 3)(*scaling)
-        golib.FindMinDist(
-            self.ptr,
-            coords_c[0],
-            coords_c[1],
-            coords_c[2],
-            offset_c[0],
-            offset_c[1],
-            offset_c[2],
-            scaling_c[0],
-            scaling_c[1],
-            scaling_c[2],
-            out_dist_ptr,
-            out_loc_x_ptr,
-            out_loc_y_ptr,
-            out_loc_z_ptr,
-        )
-
-        out_dist_python = out_dist.value
-        out_loc_python = (out_loc_x.value, out_loc_y.value, out_loc_z.value)
-        return (out_dist_python, out_loc_python)
-
-
-# golib.FindMinDist.restype = None
-# golib.FindMinDist.argtypes = [
-#     ctypes.c_int,
-#     ctypes.c_int,
-#     ctypes.c_int,
-#     array_1d_int,
-#     ctypes.c_int,
-#     ctypes.c_int,
-#     ctypes.c_int,
-#     ctypes.POINTER(ctypes.c_double),
-#     ctypes.POINTER(ctypes.c_int),
-#     ctypes.POINTER(ctypes.c_int),
-#     ctypes.POINTER(ctypes.c_int),
-# ]
-
-
-# def find_min_dist(
-#     coords: tuple[int, int, int], bin_data: NDArray[np.int_]
-# ) -> tuple[float, tuple[int, int, int]]:
-#     out_dist = ctypes.c_double(-1)
-#     out_dist_ptr = ctypes.pointer(out_dist)
-
-#     out_loc_x = ctypes.c_int(-1)
-#     out_loc_y = ctypes.c_int(-1)
-#     out_loc_z = ctypes.c_int(-1)
-#     out_loc_x_ptr = ctypes.pointer(out_loc_x)
-#     out_loc_y_ptr = ctypes.pointer(out_loc_y)
-#     out_loc_z_ptr = ctypes.pointer(out_loc_z)
-
-#     coords_c = (ctypes.c_int * 3)(*coords)
-#     shape_c = (ctypes.c_int * 3)(*bin_data.shape)
-
-#     golib.FindMinDist(
-#         coords_c[0],
-#         coords_c[1],
-#         coords_c[2],
-#         bin_data.flatten(),
-#         shape_c[0],
-#         shape_c[1],
-#         shape_c[2],
-#         out_dist_ptr,
-#         out_loc_x_ptr,
-#         out_loc_y_ptr,
-#         out_loc_z_ptr,
-#     )
-
-#     out_dist_python = out_dist.value
-#     out_loc_python = (out_loc_x.value, out_loc_y.value, out_loc_z.value)
-#     return (out_dist_python, out_loc_python)
-
-
-# n = 1024
-# array = np.zeros((n, n, n), dtype=np.int32)
-# ii, jj, kk = np.mgrid[:n, :n, :n]
-# r = n / 4
-# ii = ii - n/2
-# jj = jj - n/2
-# kk = kk - n/2
-# array[(ii**2 + jj**2 + kk**2) < r**2] = 1
-# array = array[:, :, n//2-4:n//2+4]
-
-# array[6:10, 6:10, 16] = 1
 
 img = tifffile.imread(
     "/Users/melisande.croft/Documents/Data/5639253/Multilabel_U-Net_dataset_B.subtilis/training/instance_segmentation_GT/train_18.tif"
 )
-array = np.zeros((*img.shape, 64), dtype=np.int32)
+array = np.zeros((*img.shape, 256), dtype=np.int32)
 array[img != 0, :] = 1
 
 rng = np.random.default_rng(seed=42)
-n_points = 8
+n_points = 256
 start = np.zeros(3, dtype=int)
 start[-1] = array.shape[2] // 2
 end = np.array(array.shape)
