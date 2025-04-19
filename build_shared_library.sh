52af42e930c149ff58a70113c7a96feddf3434f7
melisande-c <melisande.croft@fht.org>
2025-04-19 16:28:41 +0200

    ci(release): use glibc 2.28 for linux build

--- build_shared_library.sh
+++ build_shared_library.sh
@@ -22,6 +22,33 @@ else
     echo "Unsupported architecture '$UNAME_ARCH'"
 fi
 
+if [[ "$UNAME_OS" == "Linux" ]]; then
+    echo Getting glibc version 2.28
+
+    mkdir -p $HOME/glibc-2.28/build
+    cd $HOME/glibc-2.28
+
+    wget http://ftp.gnu.org/gnu/libc/glibc-2.28.tar.gz
+    tar -xvzf glibc-2.28.tar.gz
+    cd glibc-2.28
+    mkdir build && cd build
+
+    ../configure --prefix=$HOME/glibc-2.28/install
+    make -j$(nproc)
+    make install
+fi
+
 BUILD_NAME=octree-$OS-$ARCH.so 
 echo "Building shared library file: '$BUILD_NAME'"
-GOOS=$OS GOARCH=$ARCH CGO_ENABLED=1 go build -buildmode=c-shared -o $BUILD_NAME $SRC_FILE
+
+# with linux use glibc version 2.28 that we just downloaded
+if [[ "$UNAME_OS" == "Linux" ]]; then
+    GOOS=$OS GOARCH=$ARCH CGO_ENABLED=1 \ 
+        CC="gcc -nostdlib -Wl,--rpath=$HOME/glibc-2.28/install/lib" \
+        LD_LIBRARY_PATH="$HOME/glibc-2.28/install/lib" \
+        LIBRARY_PATH="$HOME/glibc-2.28/install/lib" \
+        C_INCLUDE_PATH="$HOME/glibc-2.28/install/include" \
+else
+    GOOS=$OS GOARCH=$ARCH CGO_ENABLED=1 \
+        go build -buildmode=c-shared -o $BUILD_NAME $SRC_FILE
+fi
